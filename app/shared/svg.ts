e145b80bb6ba0810b9b92a4598a348404648e18a
Adedeji Morifeoluwa <141498118+iamthe-nerdyDev@users.noreply.github.com>
2024-06-28 09:25:03 +0100

    feat: completed

---
+++ app/shared/svg.ts
@@ -0,0 +1,56 @@
+const svg = `
+    <svg viewBox="0 0 500 500"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:bx="https://boxy-svg.com">
+        <defs>
+            <filter id="filter0_f_703_729" x="-223.25" y="0.599487" width="828.4" height="828.401" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_703_729"/>
+            </filter>
+            <linearGradient id="paint0_linear_703_729" x1="190.951" y1="200.599" x2="190.951" y2="629" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FF7BCA"/>
+                <stop offset="1" stop-color="#FFC56F" stop-opacity="0.46"/>
+            </linearGradient>
+            <filter id="filter1_f_703_729" x="7.88306" y="75" width="513.464" height="513.464" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_703_729"/>
+            </filter>
+            <linearGradient id="paint1_linear_703_729" x1="264.615" y1="175" x2="264.615" y2="488.464" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#F22FB0"/>
+                <stop offset="1" stop-color="#F58A25" stop-opacity="0"/>
+                <stop offset="1" stop-color="#7061A3"/>
+            </linearGradient>
+            <linearGradient id="paint0_linear_896_1587" x1="13.5177" y1="0" x2="13.5177" y2="27.0355" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1, 0, 0, 1, 185.453629, 231.067154)">
+                <stop stop-color="#9327FF"/>
+                <stop offset="1" stop-color="#FFA927"/>
+            </linearGradient>
+            <style bx:fonts="Montserrat Alternates">@import url(https://fonts.googleapis.com/css2?family=Montserrat+Alternates%3Aital%2Cwght%400%2C100%3B0%2C200%3B0%2C300%3B0%2C400%3B0%2C500%3B0%2C600%3B0%2C700%3B0%2C800%3B0%2C900%3B1%2C100%3B1%2C200%3B1%2C300%3B1%2C400%3B1%2C500%3B1%2C600%3B1%2C700%3B1%2C800%3B1%2C900&amp;display=swap);</style>
+        </defs>
+        <rect x="-0.19" y="-0.493" width="501.066" height="500.267" style="stroke: rgb(0, 0, 0); stroke-width: 0px;"/>
+        <g transform="matrix(1, 0, 0, 1, 57.87114715576166, 9.002908706665039)">
+            <g filter="url(#filter0_f_703_729)" transform="matrix(0.780296, 0, 0, 0.780296, 33.87611, -81.614212)" style="">
+                <circle cx="190.951" cy="414.8" r="214.2" fill="url(#paint0_linear_703_729)" style="fill-opacity: 0.23;"/>
+            </g>
+            <g filter="url(#filter1_f_703_729)" transform="matrix(0.780296, 0, 0, 0.780296, 33.87611, -81.614212)" style="">
+                <circle cx="264.615" cy="331.732" r="156.732" fill="url(#paint1_linear_703_729)" style="fill-opacity: 0.23;"/>
+            </g>
+        </g>
+        <g transform="matrix(1.896136999130249, 0, 0, 1.896136999130249, -296.5963439941406, -383.4167175292969)" style="">
+            <g transform="matrix(1, 0, 0, 1, 0, 30.557318)">
+                <path d="M 195.524 245.855 C 202.659 241.121 206.316 254.591 210.393 256.925 C 202.779 261.927 199.601 249.59 195.524 245.855 Z" fill="#FFA927"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M 212.489 244.585 C 212.489 248.048 211.187 251.206 209.046 253.598 C 208.864 253.34 208.681 253.078 208.496 252.815 C 207.62 251.567 206.709 250.268 205.769 249.012 C 206.599 247.739 207.082 246.218 207.082 244.585 C 207.082 240.106 203.451 236.474 198.971 236.474 C 194.492 236.474 190.861 240.106 190.861 244.585 C 190.861 248.493 193.625 251.756 197.305 252.524 L 194.518 257.352 C 189.24 255.511 185.454 250.49 185.454 244.585 C 185.454 237.119 191.506 231.067 198.971 231.067 C 206.437 231.067 212.489 237.119 212.489 244.585 Z M 195.581 257.674 C 196.664 257.954 197.8 258.103 198.971 258.103 C 200.262 258.103 201.511 257.922 202.693 257.584 C 201.318 256.206 200.125 254.49 198.989 252.695 L 198.971 252.696 C 198.801 252.696 198.632 252.69 198.464 252.68 L 195.581 257.674 Z" fill="url(#paint0_linear_896_1587)"/>
+            </g>
+        </g>
+        <path style="fill: rgb(216, 216, 216); stroke: rgb(255, 255, 255); transform-origin: 249.999px 278.888px;" d="M 39.936 280.519 L 460.063 277.257" transform="matrix(1, 0, 0, 1, 0, 119.406784057621)"/>
+        <path style="fill: rgb(216, 216, 216); stroke: rgb(255, 255, 255); transform-origin: 249.999px 278.888px;" d="M 39.936 280.519 L 460.063 277.257"/>
+        <text style="fill: rgba(255, 255, 255, 0.663); font-family: 'Montserrat Alternates'; font-weight: 500; white-space: pre; font-size: 15px;" x="60.504" y="270.281">USERNAME</text>
+        <text style="fill: rgba(255, 255, 255, 0.663); font-family: 'Montserrat Alternates'; font-weight: 500; white-space: pre; font-size: 15px;" x="60.504" y="387.557">LENGTH</text>
+        <text style="fill: rgb(255, 255, 255); font-family: 'Montserrat Alternates'; font-size: 16px; font-weight: 600; white-space: pre;" x="61.306" y="306.677">[[USERNAME]]</text>
+        <text style="fill: rgb(255, 255, 255); font-family: 'Montserrat Alternates'; font-size: 16px; font-weight: 600; white-space: pre;" x="64.168" y="423.354">[[LENGTH]]</text>
+        <path style="fill: rgb(255, 255, 255); stroke: rgb(255, 255, 255);" d="M 406.089 217.591 L 407.732 460.22"/>
+    </svg>
+`;
+
+export default svg;
